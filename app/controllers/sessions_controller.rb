2b43cf89b8be0fcb5576be7ba2969e3502ed2079
Alessio <alessio@Heyzaps-iMac.local>
2015-05-06 13:03:37 -0700

    Added acct activation via email verification

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      if params[:session][:remember_me] == '1'
-        remember(user)
+      if user.activated?
+        log_in user
+        if params[:session][:remember_me] == '1'
+          remember(user)
+        else
+          forget(user)
+        end
+        redirect_back_or user_url(user)
       else
-        forget(user)
+        flash[:warning] = "Account not activated. Check email for activation link"
+        render 'new'
       end
-      redirect_back_or user_url(user)
     else
       flash.now[:danger] = "Invalid email/password"
       render 'new'
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
 
   test "invalid signup info" do
     get signup_path
@@ -11,13 +12,30 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post users_path, user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" }
     end
     assert_template "users/new"
+    assert_select "div#error_explanation"
   end
 
-  test "valid signup info" do
+  test "valid signup info with acct activation" do
     get signup_path
     assert_difference "User.count", 1 do
-      post_via_redirect users_path, user: { name: "Asdf", email: "test@test.com", password: "asdfjkl;", password_confirmation: "asdfjkl;" }
+      post users_path, user: { name: "Asdf", email: "test@test.com", password: "asdfjkl;", password_confirmation: "asdfjkl;" }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+
+    log_in_as(user)
+    assert_not is_logged_in?
+
+    get edit_account_activation_path("some invalid token blargh")
+    assert_not is_logged_in?
+
+    get edit_account_activation_path(user.activation_token, email: "some wrong email lol")
+    assert_not is_logged_in?
+
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    follow_redirect!
     assert_template "users/show"
     assert is_logged_in?
   end
